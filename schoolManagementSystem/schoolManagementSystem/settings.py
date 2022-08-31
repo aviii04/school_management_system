21227a135633d8c6f30f033e1ef3b7301aa80840
Avinash Thakur <avinash.thakur@here.com>
2022-08-31 12:15:22 +0530

    Moving secret key to private place.

--- schoolManagementSystem/schoolManagementSystem/settings.py
+++ schoolManagementSystem/schoolManagementSystem/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9$r@g+5_o+*k1%*9=kdn(%slm=iyzcx6h*7=biy56h61=%l4pw'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
